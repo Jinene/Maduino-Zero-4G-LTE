a62338a39bf1b9f931014d1a3637e69e4bf3af4f
Jnayna 🐾 <101910279+Jinene@users.noreply.github.com>
2026-01-03 18:29:35 +0100

    Create config.h

---
+++ firmware/config.h
@@ -0,0 +1,16 @@
+// config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define APN "your_apn_here"
+#define LTE_USER ""
+#define LTE_PASS ""
+#define SERVER_URL "http://YOUR_SERVER_IP:5000/update_location"
+
+#define GPS_RX_PIN 4
+#define GPS_TX_PIN 3
+#define GPS_BAUD 9600
+
+#define INTERVAL 10000 // send data every 10 seconds
+
+#endif
